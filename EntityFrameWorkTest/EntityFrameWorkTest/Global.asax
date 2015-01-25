fd02c88b20a18eb544fd0323eb33e24ebde97eea
Sequeiros <rafanetow@gmail.com>
2015-01-24 18:47:32 -0700

    FirstCommit

---
+++ EntityFrameWorkTest/EntityFrameWorkTest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EntityFrameWorkTest.MvcApplication" Language="C#" %>
---
+++ EntityFrameWorkTest/EntityFrameWorkTest/Models/Enrollment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EntityFrameWorkTest.Models
+{
+    public enum Grade
+    {
+        A,B,C,D,F
+    }
+    
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; }
+        public int CourseID { get; set; }
+        public int StudentID { get; set; }
+        public Grade? Grade { get; set; }
+        public virtual Course Courses { get; set; }
+        public virtual Student Students { get; set; }
+    }
+
+    
+}
